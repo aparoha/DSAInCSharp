080327932c4ca294e8df43da3bad9917db7dd80a
Abhay Dutt Paroha <aparoha@slb.com>
2020-12-03 14:10:32 -0600

    Added first and last occurence

--- DSAProblems/DSAProblems/Algorithms/BinarySearch/BinarySearchVariants.cs
+++ DSAProblems/DSAProblems/Algorithms/BinarySearch/BinarySearchVariants.cs
@@ -6,10 +6,10 @@ namespace DSAProblems.Algorithms.BinarySearch
     {
         static void Main(string[] args)
         {
-            int[] arr = { 2, 3, 4, 10, 40 }; 
+            int[] arr = { 1, 2, 3, 4, 4, 5, 6, 7 }; 
             int x = 10;
 
-            Console.WriteLine(StandardBsRecursive(arr, 0, 9, 10));
+            Console.WriteLine(LastOccurence(arr, 4));
 
             Console.ReadLine();
         }
@@ -55,6 +55,69 @@ namespace DSAProblems.Algorithms.BinarySearch
   
             // We reach here when element is not present in array 
             return -1; 
+        }
+
+        //Duplicate elements
+        //The return value of binary search is quite binary - either it gives index of the value searched for or -1
+        //It would be nice to handle duplicates in a way that is more useful. Perhaps getting information like the index of:
+        //1.The first occurrence of a value
+        //2.The last occurrence of a value or in the case where the value we’re searching for doesn’t exist, the index of the:
+        //3.Closest value
+        //4.Next largest value
+        //5.Next smallest value
+
+        //Approximate Matches
+        //Binary search can be used to compute, for a given value
+        //1.Its rank (the number of smaller elements)
+        //2.Predecessor (next-smallest element),
+        //3.Successor (next-largest element)
+        //4.Nearest neighbor
+        //Example - Target = 5 (5 is not present in array)
+
+        //                  | (Predecessor or nearest neighbor)
+        //      1   2   3   4   7   8   10  11  13  14  15        
+        //      [  Rank = 4 ]   | (Successor)
+
+
+        public static int FirstOccurence(int[] arr, int target)
+        {
+            int low = 0, high = arr.Length - 1;
+            while (low <= high)
+            {
+                // same as (low + high) / 2, but prevents int overflows
+                int mid = low + (high - low) / 2; 
+  
+                if (target > arr[mid]) {
+                    low = mid + 1;
+                } else if (target < arr[mid]) {
+                    high = mid - 1;
+                } else if (low != mid) {
+                    high = mid;
+                } else {
+                    return mid;
+                }
+            }
+            return -1;
+        }
+
+        public static int LastOccurence(int[] arr, int target)
+        {
+            int low = 0, high = arr.Length - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if (target > arr[mid]) {
+                    low = mid + 1;
+                } else if (target < arr[mid]) {
+                    high = mid - 1;
+                } else if (low != mid) { 
+                    low = mid;
+                } else {
+                    return arr[high] == target ? high : low;
+                }
+            }
+            return -1;
         }
     }
 }
