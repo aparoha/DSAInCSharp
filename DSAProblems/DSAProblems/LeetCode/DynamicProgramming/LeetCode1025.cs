9759f6c3a6bf1a646539d16f74d2da752ed73ed8
Abhay Dutt Paroha <aparoha@slb.com>
2020-12-11 16:49:22 -0600

    Improved solution

--- DSAProblems/DSAProblems/LeetCode/DynamicProgramming/LeetCode1025.cs
+++ DSAProblems/DSAProblems/LeetCode/DynamicProgramming/LeetCode1025.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace DSAProblems.LeetCode.DynamicProgramming
 {
+    //1025 - Divisor Game
+
+    //Alice and Bob take turns playing a game, with Alice starting first.
+    //
+    //Initially, there is a number N on the chalkboard.  On each player's turn, that player makes a move consisting of:
+    //
+    //Choosing any x with 0 < x < N and N % x == 0.
+    //Replacing the number N on the chalkboard with N - x.
+    //Also, if a player cannot make a move, they lose the game.
+    //
+    //Return True if and only if Alice wins the game, assuming both players play optimally.
     class LeetCode1025
     {
         //Recursion Brute Force
@@ -32,15 +39,17 @@ namespace DSAProblems.LeetCode.DynamicProgramming
                 return result;
             }
             if(N == 1) return false;
-            for(int x = 1; x < N; x++){
-                if(N % x == 0 && !DivisorGameMemoHelper(N - x, memo))
+            bool retVal = false;
+            for(int x = 1; x < N; x++)
+            {
+                if (N % x == 0 && !DivisorGameMemoHelper(N - x, memo))
                 {
-                    memo[N] = true;
-                    return memo[N];
-                }   
+                    retVal = true;
+                    break;
+                }
             }
-            memo[N] = false;
-            return false;
+            memo[N] = retVal;
+            return memo[N];
         }
     }
 }
