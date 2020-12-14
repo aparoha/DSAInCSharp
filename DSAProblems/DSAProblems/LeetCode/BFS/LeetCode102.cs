c18b835e18fbeda12f3c97dd9b1dc8633f9fc07a
Abhay Dutt Paroha <aparoha@slb.com>
2020-12-14 09:16:39 -0600

    Added LC1161

--- DSAProblems/DSAProblems/LeetCode/BFS/LeetCode102.cs
+++ DSAProblems/DSAProblems/LeetCode/BFS/LeetCode102.cs
@@ -19,47 +19,37 @@ namespace DSAProblems.LeetCode.BFS
     //    [15,7]
     //    ]
 
-     public class TreeNode {
-          public int val;
-          public TreeNode left;
-          public TreeNode right;
-          public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
-              this.val = val;
-              this.left = left;
-              this.right = right;
-          }
-      }
-
     class LeetCode102
     {
         public IList<IList<int>> LevelOrder(TreeNode root) {
         
-            if(root == null) return new List<IList<int>>();
+            if(root == null) 
+                return new List<IList<int>>();
         
-            var res = new List<IList<int>>();
+            var result = new List<IList<int>>();
             var queue = new Queue<TreeNode>();
             queue.Enqueue(root);
+
             while (queue.Any()) 
             {
                 var size = queue.Count;
-                var one = new List<int>();
+                var currentLevel = new List<int>();
                 for (int i = 0; i < size; i++)
                 {
-                    var top = queue.Dequeue();
-                    if (top != null)
+                    var frontNode = queue.Dequeue();
+                    if (frontNode != null)
                     {
-                        one.Add(top.val);
-                        queue.Enqueue(top.left);
-                        queue.Enqueue(top.right);
+                        currentLevel.Add(frontNode.val);
+                        queue.Enqueue(frontNode.left);
+                        queue.Enqueue(frontNode.right);
                     }
                 }
-                if (one.Any())
+                if (currentLevel.Any())
                 {
-                    res.Add(one);
+                    result.Add(currentLevel);
                 }
             }
-            return res;
-        
+            return result;    
         }
     }
 }
