839a46589ff5f98d3d7098668106fb402f4afbc8
Abhay Dutt Paroha <aparoha@slb.com>
2020-12-14 12:49:40 -0600

    Added more solution

---
+++ DSAProblems/DSAProblems/LeetCode/BFS/LeetCode104.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAProblems.LeetCode.BFS
+{
+//        104. Maximum Depth of Binary Tree
+//        Given the root of a binary tree, return its maximum depth.
+//
+//        A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+// 
+//        Input: root = [3,9,20,null,null,15,7]
+//        Output: 3
+//
+//        Input: root = [1,null,2]
+//        Output: 2
+//
+//        Input: root = []
+//        Output: 0
+//
+//        Input: root = [0]
+//        Output: 1
+        
+    class LeetCode104
+    {
+        public int MaxDepthIterative(TreeNode root) {
+        
+            int maxDepth = 0;
+            if(root == null) return maxDepth;
+        
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+        
+            while(queue.Any()){
+                int size = queue.Count;
+                for(int i = 0; i < size; i++){
+                    TreeNode current = queue.Dequeue();
+                    if (current.left != null) queue.Enqueue(current.left);
+                    if (current.right != null) queue.Enqueue(current.right);
+                }
+                maxDepth++;
+            }
+        
+            return maxDepth;
+        }
+
+        public int MaxDepthR(TreeNode root) {
+        
+            if(root == null) return 0;
+            return Math.Max(MaxDepthR(root.left), MaxDepthR(root.right)) + 1;
+        }
+    }
+}
---
+++ DSAProblems/DSAProblems/LeetCode/BFS/LeetCode993.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAProblems.LeetCode.BFS
+{
+    class LeetCode993
+    {
+        TreeNode _xParent;
+        TreeNode _yParent;
+        int _xDepth = -1, _yDepth = -1;
+
+        public bool IsCousinsIterative(TreeNode root, int x, int y) {
+        
+            if(root == null) return false;
+        
+            Dictionary<int, Tuple<int?, int>> parentMap = new Dictionary<int, Tuple<int?, int>>();
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            parentMap.Add(root.val, Tuple.Create<int?, int>(null, 0));
+        
+            while(queue.Any()){
+                int size = queue.Count;
+                for(int i = 0; i < size; i++){
+                    TreeNode current = queue.Dequeue();
+                    if(current.left != null){
+                        queue.Enqueue(current.left);
+                        parentMap.Add(current.left.val, Tuple.Create<int?, int>(current.val, parentMap[current.val].Item2 + 1));
+                    }
+                    if(current.right != null){
+                        queue.Enqueue(current.right);
+                        parentMap.Add(current.right.val, Tuple.Create<int?, int>(current.val, parentMap[current.val].Item2 + 1));
+                    }
+                }      
+            }
+        
+            return (parentMap[x].Item1 != parentMap[y].Item1 && parentMap[x].Item2 == parentMap[y].Item2);
+        
+        }
+
+        public bool IsCousinsR(TreeNode root, int x, int y) {
+            GetDepthAndParent(root, x, y, 0, null);
+            return _xDepth == _yDepth && _xParent != _yParent;
+        }
+        //get both the depth and parent for x and y
+        public void GetDepthAndParent(TreeNode root, int x, int y, int depth, TreeNode parent){
+            if(root == null){
+                return;
+            }
+            if(root.val == x){
+                _xParent = parent;
+                _xDepth = depth;
+            }else if(root.val == y){
+                _yParent = parent;
+                _yDepth = depth;
+            }       
+            GetDepthAndParent(root.left, x, y, depth + 1, root);
+            GetDepthAndParent(root.right, x, y, depth + 1, root);
+        }
+    }
+}
